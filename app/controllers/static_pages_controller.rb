72535dec155b411482719da62a26a0cd04ddae64
Xie Guangzhi <frankxgz@gmail.com>
2012-07-02 16:08:46 +0800

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def work
+  end
+
+  def academic
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 PersonalSite::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/work"
+
+  get "static_pages/academic"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
